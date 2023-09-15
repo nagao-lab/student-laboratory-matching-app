8aaa6c6feeb5f409cd8c1c5c8fa1598e84d9e04e
RyoMatsumoto <matsumoto-ryo-bw@ynu.jp>
2023-09-15 12:15:22 +0900

    add : メッセージ一覧のmockdata

--- student-frontend/src/features/messages/mock/messages.ts
+++ student-frontend/src/features/messages/mock/messages.ts
@@ -1,2 +1,47 @@
 // TODO メッセージ一覧ページ用mockデータ : Messageの型を定義し,exportする
-// TODO メッセージ一覧ページ用mockデータ : mockデータを定義し,exportする
+
+export type Message = {
+    student_laboratory: StudentLaboratory;
+    laboratory: Laboratory;
+    university: University;
+  };
+  
+  export type StudentLaboratory = {
+    status: string;
+  }
+
+  export type Laboratory = {
+    name: string;
+  };
+
+  export type University = {
+    name: string
+  }
+  
+  // TODO メッセージ一覧ページ用mockデータ : mockデータを定義し,exportする
+  
+  export const MockMessages: Message[] = [
+    {
+      student_laboratory: {
+        status: 'LIKE_FROM_BOTH'
+      },
+      laboratory: {
+        name: '研究室1'
+      },
+      university: {
+        name: '大学1'
+      }
+    },
+    {
+      student_laboratory: {
+        status: 'LIKE_FROM_BOTH'
+      },
+      laboratory: {
+        name: '研究室2'
+      },
+      university: {
+        name: '大学2'
+      },
+    }
+  ];
+  
