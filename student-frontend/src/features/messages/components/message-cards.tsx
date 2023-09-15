c71591ef4f47a52b4a42ae94108d0673c7e951c6
RyoMatsumoto <matsumoto-ryo-bw@ynu.jp>
2023-09-15 14:41:51 +0900

    add : メッセージ一覧の表示

--- student-frontend/src/features/messages/components/message-cards.tsx
+++ student-frontend/src/features/messages/components/message-cards.tsx
@@ -1,4 +1,44 @@
 // TODO メッセージ一覧ページ : MessageCardsコンポーネントを作成
+
+
+
+import { Box, Button, Card, CardContent, CardActions, Typography } from "@mui/material";
+import { Message } from "../mock/messages";
+
 // TODO メッセージ一覧ページ : MessageCardsコンポーネント用のPropsを定義
+
+type Props = {
+  messages: Message[];
+};
+
 // TODO メッセージ一覧ページ : MessageCardsではMuiのBoxを利用する
 // TODO メッセージ一覧ページ : Box内でmap関数を利用してMuiのCardを挿入する
+
+export const MessageCards = ({ messages }: Props) => {
+  return (
+    <Box>
+      {messages
+      .filter((item) => {
+        return item.studentLaboratory.status === 'LIKE_FROM_BOTH';
+      })
+      
+      .map((message, i) => (
+        <Card key={i} sx={{ minWidth: 275, m: 5}}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              {message.laboratory.name}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {message.university.name}
+           </Typography>
+        
+          </CardContent>
+          <CardActions>
+            <Button size="small">やりとりを見る</Button>
+          </CardActions>
+        </Card>
+      ))}
+    </Box>
+
+  );
+};
