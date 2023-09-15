d8626e278009d5ca544d317ad46c1dbdc4ffe606
yuhisato <yuhi120101@gmail.com>
2023-09-15 10:49:14 +0900

    add : mockの例

--- student-frontend/src/features/laboratories/mock/laboratories.ts
+++ student-frontend/src/features/laboratories/mock/laboratories.ts
@@ -3,17 +3,40 @@
 export type Laboratory = {
   name: string;
   comment: string;
+  university: University;
+  major: Major;
 };
+
+export type University = {
+  name: string
+}
+
+export type Major = {
+  name: string
+}
 
 // TODO 研究室一覧ページ用mockデータ : mockデータを定義し,exportする
 
+
 export const MockLaboratories: Laboratory[] = [
   {
-    name: "長尾研究室",
-    comment: "長尾研究室は、自然言語処理を中心とした研究を行っています。",
+    name: '研究室1',
+    comment: '研究室1のコメント',
+    university: {
+      name: '大学1'
+    },
+    major: {
+      name: '学部1'
+    }
   },
   {
-    name: "白川研究室",
-    comment: "白川研究室は、自然言語処理を中心とした研究を行っています。",
-  },
+    name: '研究室2',
+    comment: '研究室2のコメント',
+    university: {
+      name: '大学2'
+    },
+    major: {
+      name: '学部2'
+    }
+  }
 ];
