c016793ace174ff927ae12954170395634bb9665
fairy26 <yoshida-yosei-vx@ynu.jp>
2023-09-15 06:55:38 +0000

    都道府県のテーブルを作成

--- backend/migrate/migrate.go
+++ backend/migrate/migrate.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"student-laboratory-matching-app/db"
+	"student-laboratory-matching-app/model"
 )
 
 func main() {
 	dbConn := db.NewDB()
 	defer fmt.Println("Successfully Migrated")
 	defer db.CloseDB(dbConn)
-	dbConn.AutoMigrate()
+	dbConn.AutoMigrate(&model.Prefecture{})
 }
