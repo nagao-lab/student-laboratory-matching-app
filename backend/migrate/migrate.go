68ef25811adef8375a5f8583da9bc474538ca9da
fairy26 <yoshida-yosei-vx@ynu.jp>
2023-09-15 06:54:54 +0000

    マイグレーションの関数を追加

---
+++ backend/migrate/migrate.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"student-laboratory-matching-app/db"
+)
+
+func main() {
+	dbConn := db.NewDB()
+	defer fmt.Println("Successfully Migrated")
+	defer db.CloseDB(dbConn)
+	dbConn.AutoMigrate()
+}
