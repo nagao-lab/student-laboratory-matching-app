402771d3cacb6a9b8e960e2aa3e1ef28f7a065ca
yuhisato <yuhi120101@gmail.com>
2023-09-16 13:02:54 +0900

    add : student-image

--- student-frontend/src/features/student/components/student-image.tsx
+++ student-frontend/src/features/student/components/student-image.tsx
@@ -1,1 +1,18 @@
 // TODO 学生ページ :
+
+import Image from "next/image";
+
+type Props = {
+  image_url: string;
+};
+
+export const StudentImage = ({ image_url }: Props) => {
+  return (
+    <Image
+      src={image_url}
+      width={500}
+      height={500}
+      alt="Picture of the student"
+    />
+  );
+};
--- student-frontend/src/features/student/index.tsx
+++ student-frontend/src/features/student/index.tsx
@@ -1,5 +1,6 @@
 // TODO 学生ページ用mockデータ : mockデータ用の型定義をexportする
 // TODO 学生ページ用mockデータ : mockデータをexportする
-export { MockStudents} from "./mock/student";
+export { MockStudents } from "./mock/student";
+export { StudentImage } from "./components/student-image";
 // TODO 学生ページ（mock） : StudentCommentをexportする
 // TODO 学生ページ（mock） : StudentInterestをexportする
