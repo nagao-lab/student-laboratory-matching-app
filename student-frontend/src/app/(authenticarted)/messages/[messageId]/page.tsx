4d2b6cf163844c462a19c46674d9fe0516f4e859
yuhisato <yuhi120101@gmail.com>
2023-09-16 10:24:07 +0900

    fix : 研究室詳細,メッセージ詳細のpage.tsx

--- student-frontend/src/app/(authenticarted)/messages/[messageId]/page.tsx
+++ student-frontend/src/app/(authenticarted)/messages/[messageId]/page.tsx
@@ -1,1 +1,10 @@
 // TODO メッセージ詳細ページ（mock） : MessageContentコンポーントをレンダリングする
+
+import { Box } from "@mui/material";
+import { NextPage } from "next";
+
+const Page: NextPage = () => {
+  return <Box>message detail</Box>;
+};
+
+export default Page;
