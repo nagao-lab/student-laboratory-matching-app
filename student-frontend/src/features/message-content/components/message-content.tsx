4fd4a9c2d9a6730176746dddf221356f1c5cc8a5
yu00h <oohashi-yuuto-zc@ynu.jp>
2023-09-17 17:26:12 +0900

    add: MessageDetailコンポーネント,メッセージ詳細ページ追加

--- student-frontend/src/features/message-content/components/message-content.tsx
+++ student-frontend/src/features/message-content/components/message-content.tsx
@@ -1,4 +1,6 @@
+
 // TODO メッセージ詳細ページ（mock） : コンポーネントのPropsの型を定義する
+
 // TODO メッセージ詳細ページ（mock） : MessageDetailコンポーネントを定義する
 // TODO メッセージ詳細ページ（mock） : MessageDetailコンポーネントにMockMessageDetailを渡す
 // TODO メッセージ詳細ページ（mock） : MessageDetail内ではBoxを利用する
--- student-frontend/src/features/message-content/mock/message-content.ts
+++ student-frontend/src/features/message-content/mock/message-content.ts
@@ -1,4 +1,5 @@
 // TODO メッセージ詳細ページ用mockデータ : Messageの型を定義し,exportする
+
 // TODO メッセージ詳細ページ用mockデータ : mockデータMessageContentを定義し,exportする
 
 export type Message = {
