b889b5347b5f9f4af2d8c6f05f7411dffa3a7288
Ririka248 <hamana-ririka-hp@ynu.jp>
2023-09-18 16:08:00 +0900

    add:学生ページ

--- laboratory-frontend/src/features/laboratory/components/student-comment.tsx
+++ laboratory-frontend/src/features/laboratory/components/student-comment.tsx
@@ -1,3 +1,21 @@
 // TODO 学生ページ : StudentCommentコンポーネントを作成する
 // TODO 学生ページ : StudentCommentコンポーネント用のPropsの型を定義する
 // TODO 学生ページ : StudentCommentではMuiのBoxを利用する
+
+import { Box, Card, CardContent } from "@mui/material";
+
+type Props = {
+    comment: string;
+  };
+
+  export const StudentComment = ({ comment }: Props) => {
+    return (
+        <Box>
+            <Card>
+                <CardContent>
+                    {comment}
+                </CardContent>
+            </Card>
+        </Box>
+    )
+  }
