ab6e347a44410435e413c70b266569f01d64a1f7
yuhisato <yuhi120101@gmail.com>
2023-09-18 14:37:06 +0900

    fix : toggleコンポーネント削除

--- student-frontend/src/features/laboratories/components/laboratory-form.tsx
+++ student-frontend/src/features/laboratories/components/laboratory-form.tsx
@@ -1,20 +1,28 @@
-import { Checkbox, FormControlLabel, FormGroup, TextField } from "@mui/material";
+import {
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  TextField,
+} from "@mui/material";
 type Props = {
-    setFilterVal: (val: string) => void
-    setToggle: (val: boolean) => void
-  };
+  setFilterVal: (val: string) => void;
+};
 
-export const LaboratoryForm = ({ setFilterVal, setToggle }: Props) => {
-    const toggleComponent = () => {
-        setToggle(prev => !prev)
-    }
-    
-    return(
+export const LaboratoryForm = ({ setFilterVal }: Props) => {
+  return (
     <>
-      <TextField label="検索ワード" sx={{ minWidth: 250, m: 1.25}} onChange={(e) => setFilterVal(e.target.value)}/>
+      <TextField
+        label="検索ワード"
+        sx={{ minWidth: 250, m: 1.25 }}
+        onChange={(e) => setFilterVal(e.target.value)}
+      />
       <FormGroup>
-        <FormControlLabel sx={{m: 1.25}} control={<Checkbox />} label="「興味あり」のみ表示" onChange={() => toggleComponent()}/>
+        <FormControlLabel
+          sx={{ m: 1.25 }}
+          control={<Checkbox />}
+          label="「興味あり」のみ表示"
+        />
       </FormGroup>
-</>
-    )
-}
+    </>
+  );
+};
