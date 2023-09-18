db50d3a9df58ac0ac3103e5aebc1557f240e3dae
fairy26 <yoshida-yosei-vx@ynu.jp>
2023-09-18 07:25:53 +0000

    refactor: migrateファイルの記述を削除

--- backend/migrate/migrate.go
+++ backend/migrate/migrate.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"student-laboratory-matching-app/db"
 	"student-laboratory-matching-app/model"
-	"time"
 )
 
 func main() {
@@ -13,25 +12,4 @@ func main() {
 	defer db.CloseDB(dbConn)
 
 	dbConn.AutoMigrate(&model.Prefecture{})
-	dbConn.AutoMigrate(&model.Laboratory_Major{})
-	dbConn.AutoMigrate(&model.Laboratory{})
-	dbConn.AutoMigrate(&model.Major{})
-	dbConn.AutoMigrate(&model.Message{})
-	dbConn.AutoMigrate(&model.Student{})
-	dbConn.AutoMigrate(&model.Student_Laboratory{})
-	dbConn.AutoMigrate(&model.Student_Major{})
-	dbConn.AutoMigrate(&model.University{})
-
-	new_ac_st := db.CreateModelStudent(*dbConn, "suzuki", "suzuki@i.com", 0, 3, "fire", "water", time.Now(), 3.4, "i/url", 1, "passN", 2)
-	err := db.SignupStudent(dbConn, new_ac_st, "Tokyo Daigaku", "Tokyo", "info")
-	if err != nil {
-		fmt.Printf("%v\n", err)
-	} else {
-		fmt.Println("Success:signin")
-	}
-
-	_, err = db.LoginStudent(dbConn, "suzuki@i.com", "passN")
-	if err != nil {
-		fmt.Printf("%v\n", err)
-	}
 }
