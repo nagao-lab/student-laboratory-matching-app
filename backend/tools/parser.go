f3da96785680ac217c163a32733ba0b2478bb917
fairy26 <yoshida-yosei-vx@ynu.jp>
2023-09-21 08:10:25 +0000

    add: uint→stringのparserを追加

--- backend/tools/parser.go
+++ backend/tools/parser.go
@@ -6,3 +6,7 @@ func ParseStringToUint(s string) uint {
 	numUint64, _ := strconv.ParseUint(s, 10, 64)
 	return uint(numUint64)
 }
+
+func ParseUintToString(u uint) string {
+	return strconv.FormatUint(uint64(u), 10)
+}
