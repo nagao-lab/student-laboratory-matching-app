2f3073d768f2b31225329cbd2f9e7c6d82ac21e9
yuhisato <yuhi120101@gmail.com>
2023-09-22 16:29:41 +0900

    /student消した

--- student-frontend/src/app/(authenticarted)/student/page.tsx
+++ student-frontend/src/app/(authenticarted)/student/page.tsx
@@ -1,6 +1,6 @@
-import { StudentInterest } from "@/features/student/components/student-interest";
-import { StudentComment } from "@/features/student/components/student-comment";
-import { StudentDetail } from "@/features/student/components/student-detail";
+// import { StudentInterest } from "@/features/student/components/student-interest";
+// import { StudentComment } from "@/features/student/components/student-comment";
+// import { StudentDetail } from "@/features/student/components/student-detail";
 import { StudentImage } from "@/features/student/components/student-image";
 import { MockStudents } from "@/features/student";
 import { Box, Stack } from "@mui/material";
@@ -16,9 +16,9 @@ const Page: NextPage = () => {
         <Stack direction="row" spacing={1.0}>
           <StudentImage student={MockStudents[num]} />
           <Stack sx={{ width: 1 }} spacing={1.0}>
-            <StudentDetail student={MockStudents[num]} />
+            {/* <StudentDetail student={MockStudents[num]} />
             <StudentComment student={MockStudents[num]} />
-            <StudentInterest student={MockStudents[num]} />
+            <StudentInterest student={MockStudents[num]} /> */}
           </Stack>
         </Stack>
       </Box>
