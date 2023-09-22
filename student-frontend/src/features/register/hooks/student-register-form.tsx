c66f408f34c16686fb0ff2dace12290123f6e6ef
yuhisato <yuhi120101@gmail.com>
2023-09-22 15:55:15 +0900

    fix : CI/CD対応

--- student-frontend/src/features/register/hooks/student-register-form.tsx
+++ student-frontend/src/features/register/hooks/student-register-form.tsx
@@ -2,8 +2,12 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export const useRegisterForm = () => {
   const router = useRouter();
@@ -19,40 +23,48 @@ export const useRegisterForm = () => {
   const [file, setFile] = useState<File | null>(null);
   const [status, setStatus] = useState<number | null>();
 
-  const region = process.env.NEXT_PUBLIC_REGION ? process.env.NEXT_PUBLIC_REGION : '';
-  const accessKeyId = process.env.NEXT_PUBLIC_ACCESS_KEY_ID ? process.env.NEXT_PUBLIC_ACCESS_KEY_ID : ''
-  const secretAccessKey = process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY ? process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY : ''
+  const region = process.env.NEXT_PUBLIC_REGION
+    ? process.env.NEXT_PUBLIC_REGION
+    : "";
+  const accessKeyId = process.env.NEXT_PUBLIC_ACCESS_KEY_ID
+    ? process.env.NEXT_PUBLIC_ACCESS_KEY_ID
+    : "";
+  const secretAccessKey = process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY
+    ? process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY
+    : "";
 
   const s3 = new S3Client({
     region: region,
     credentials: {
-    accessKeyId: accessKeyId,
-    secretAccessKey: secretAccessKey
-    }
+      accessKeyId: accessKeyId,
+      secretAccessKey: secretAccessKey,
+    },
   });
 
   async function uploadImage(file: File) {
     try {
-      const key = `${Date.now()}-${file.name}`
-      const bucket = process.env.NEXT_PUBLIC_BUCKET_NAME ? process.env.NEXT_PUBLIC_BUCKET_NAME : '';
+      const key = `${Date.now()}-${file.name}`;
+      const bucket = process.env.NEXT_PUBLIC_BUCKET_NAME
+        ? process.env.NEXT_PUBLIC_BUCKET_NAME
+        : "";
       await s3.send(
         new PutObjectCommand({
           Body: file,
           Bucket: bucket,
-          Key: key
+          Key: key,
         })
       );
       const command = new GetObjectCommand({
         Bucket: bucket,
-        Key: key
-      })
-      const url = await getSignedUrl(s3, command)
+        Key: key,
+      });
+      const url = await getSignedUrl(s3, command);
       return url;
     } catch (error) {
-      console.log(error)
+      console.log(error);
       return "";
     }
-   };
+  }
   const handleSubmit = async () => {
     if (
       !name ||
@@ -67,19 +79,32 @@ export const useRegisterForm = () => {
       !file ||
       status === null
     ) {
-      window.alert("すべての項目を入力してください")
-      console.log(name, gender, university, grade, comment, interest, birthday, prefecture, gpa, file, status ); 
+      window.alert("すべての項目を入力してください");
+      console.log(
+        name,
+        gender,
+        university,
+        grade,
+        comment,
+        interest,
+        birthday,
+        prefecture,
+        gpa,
+        file,
+        status
+      );
       return;
     }
-    
-    const url =  await uploadImage(file).then((url) => {
-      return url;
-    })
-    .catch(() => {
-      return "";
-    });
 
-    console.log(url)
+    const url = await uploadImage(file)
+      .then((url) => {
+        return url;
+      })
+      .catch(() => {
+        return "";
+      });
+
+    console.log(url);
     router.push("/");
   };
 
--- student-frontend/src/features/student/components/student-image.tsx
+++ student-frontend/src/features/student/components/student-image.tsx
@@ -1,35 +1,42 @@
-// TODO 学生ページ :
-
-import { Box, Card, CardContent, Stack, Typography, IconButton } from "@mui/material";
-import { Edit } from '@mui/icons-material';
+import {
+  Box,
+  Card,
+  CardContent,
+  Stack,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import { Edit } from "@mui/icons-material";
 import { Student } from "../mock/student";
-import Image from 'next/image'
+import Image from "next/image";
 
 type Props = {
-    student: Student;
-  };
+  student: Student;
+};
 
-  export const StudentImage = ({ student }: Props) => {
-    return (
-        
-        <Box>
-            <Card>
-                <CardContent>
-                    <Stack direction="row" justifyContent="space-between">
-                        <Typography></Typography>
-                        <IconButton sx={{right: 'right'}}>
-                            <Edit sx={{ fontSize: 14 }} />
-                        </IconButton>
-                    </Stack>
-                    <Image src={student.image_url}
-                    width={100}
-                    height={100} 
-                    style={{ objectFit: 'contain' }}
-                    alt="Picture of the author"
-                    />
-                    <Typography align="center" fontSize={25}>{student.name}</Typography>
-                </CardContent>   
-            </Card>  
-        </Box>
-    )
-  }
+export const StudentImage = ({ student }: Props) => {
+  return (
+    <Box>
+      <Card>
+        <CardContent>
+          <Stack direction="row" justifyContent="space-between">
+            <Typography></Typography>
+            <IconButton sx={{ right: "right" }}>
+              <Edit sx={{ fontSize: 14 }} />
+            </IconButton>
+          </Stack>
+          <Image
+            src={student.image_url}
+            width={100}
+            height={100}
+            style={{ objectFit: "contain" }}
+            alt="Picture of the author"
+          />
+          <Typography align="center" fontSize={25}>
+            {student.name}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
