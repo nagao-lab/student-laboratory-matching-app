e562ae6e8fff719d6916f909e8ca186c92646fce
yuhisato <yuhi120101@gmail.com>
2023-09-22 16:31:48 +0900

    美醜背８位

--- student-frontend/src/app/(authenticarted)/student/page.tsx
+++ student-frontend/src/app/(authenticarted)/student/page.tsx
@@ -1,20 +1,20 @@
 // import { StudentInterest } from "@/features/student/components/student-interest";
 // import { StudentComment } from "@/features/student/components/student-comment";
 // import { StudentDetail } from "@/features/student/components/student-detail";
-import { StudentImage } from "@/features/student/components/student-image";
-import { MockStudents } from "@/features/student";
+// import { StudentImage } from "@/features/student/components/student-image";
+// import { MockStudents } from "@/features/student";
 import { Box, Stack } from "@mui/material";
 import { NextPage } from "next";
 import { StudentProvider } from "@/features/student/providers/student";
 
-var num = 0;
+// var num = 0;
 
 const Page: NextPage = () => {
   return (
     <StudentProvider>
       <Box>
         <Stack direction="row" spacing={1.0}>
-          <StudentImage student={MockStudents[num]} />
+          {/* <StudentImage student={MockStudents[num]} /> */}
           <Stack sx={{ width: 1 }} spacing={1.0}>
             {/* <StudentDetail student={MockStudents[num]} />
             <StudentComment student={MockStudents[num]} />
